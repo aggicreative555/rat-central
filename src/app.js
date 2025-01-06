d9c08c925e45e4beec4d3246619f9e251c84b176
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-06 19:50:25 +0100

    chore(tailwind): add tailwind and config
    added config, edited tailwind, added prettier plugin #1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-console.log("Current pathname:", window.location.pathname)
+console.log("Current pathname:", window.location.pathname);
 
 import "./css/style.css";
 import router from "./js/router/index.js";
 import { setLogoutListener } from "./js/ui/global/logout";
 
 document.addEventListener("DOMContentLoaded", () => {
-    try {
-        setLogoutListener();
-    } catch (error) {
-        console.error("Error setting up logout listener:", error);
-    }
+  try {
+    setLogoutListener();
+  } catch (error) {
+    console.error("Error setting up logout listener:", error);
+  }
 });
 
 await router(window.location.pathname);
-
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.html", "./js/**/*.js", "!./node_modules/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+

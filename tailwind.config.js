775a5c9ec4a16267d0837a241a6feb8fbda57420
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-07 22:15:40 +0100

    chore(tailwind): configure tailwind, add jit
    had a postcss issue, fixed
    added jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./**/*.html", "./js/**/*.js", "!./node_modules/**/*"],
+  mode: 'jit',
+  content: [
+    "./**/*.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "#",
+          100: "#",
+          200: "#",
+          300: "#",
+          400: "#",
+          500: "#2196F3", // Default brand colour
+          600: "#1E88E5",
+          700: "#1976D2",
+          800: "#1565C0",
+          900: "#0D47A1", // Dark brand colour
+        },
+      },
+    },
   },
+
+  darkMode: "selector",
   plugins: [],
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
 
 export default defineConfig({
   appType: "mpa",

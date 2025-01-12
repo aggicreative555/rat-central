c3cf00e8bfd18f4e0e47f4500ee8f1796bdb9803
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-12 19:25:21 +0100

    chore: remove empty spaces

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ console.log("Current pathname:", window.location.pathname);
 import "./css/style.css";
 import router from "./js/router/index.js";
 import { setLogoutListener } from "./js/ui/global/logout";
+import { setNavToggler } from "./js/utilities/navToggler.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   try {
-    setLogoutListener();
+    setNavToggler();
   } catch (error) {
-    console.error("Error setting up logout listener:", error);
+    console.error("Error setting up listeners", error);
   }
 });
 
--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,5 +1,1 @@
-const modeToggler = document.querySelector("#mode-toggler");
-	
-modeToggler.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
-});
+

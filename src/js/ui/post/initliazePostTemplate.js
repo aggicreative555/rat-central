24e0252c4914e7c41dfb7af7b377ffdb1f0d09dd
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-25 15:43:09 +0100

    refractor(postTemplate): refractor initPostTemplate  function
    minified function for modularity and dry #9 #19

--- src/js/ui/post/initliazePostTemplate.js
+++ src/js/ui/post/initliazePostTemplate.js
@@ -12,28 +12,14 @@ import { renderMultiplePosts } from "../../ui/post/postTemplate";
  *
  */
 
-export async function initializePostsPage() {
-  const params = new URLSearchParams(window.location.search);
-  const id = params.get("id");
-
-  const postsContainer = document.querySelector("#postsContainer");
-
-  if (id) {
-    try {
-      const post = await readPost(id);
-      renderPostTemplate(post, postsContainer);
-    } catch (error) {
-      console.error("Error fetching post:", error);
-      postsContainer.innerHTML = `<p>Failed to load the post. Please try again later or refresh the page.</p>`;
-    }
-  } else {
-    try {
-      const { data: posts } = await readMultiplePosts(12, 1);
-
-      renderMultiplePosts(posts, postsContainer);
-    } catch (error) {
-      console.error("Error fetching multiple posts:", error);
-      postsContainer.innerHTML = `<p>Failed to load posts. Please try again later or refresh the page.</p>`;
-    }
+export async function initializePostsPage(postsContainer) {
+  try {
+    const { data: posts } = await readMultiplePosts(12, 1);
+    renderMultiplePosts(posts, postsContainer);
+    
+  } catch (error) {
+    console.error("Error fetching multiple posts:", error);
+    postsContainer.innerHTML = `<p>Failed to load posts. Please try again later or refresh the page.</p>`;
   }
+
 }

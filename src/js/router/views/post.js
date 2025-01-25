d1d40822e1f9b9c5490f81de0c40dfb2499b436d
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-25 15:41:39 +0100

    fix(posts): refractor init posts function
    add conditions depending on whether the user is on /post/ or /post/?id= page.

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,22 +1,37 @@
 import { initializePostsPage } from "../../ui/post/initliazePostTemplate";
 import { initializeSearch } from "../../ui/post/initializeSearch";
 import { renderPostTemplate } from "../../ui/post/postTemplate";
+import { readPost } from "../../api/post/read";
+import { renderSinglePost } from "../../ui/post/postTemplate";
 
-export function initializePostFunctions() {
+export async function initializePostFunctions() {
 
   try {
     const urlParams = new URLSearchParams(window.location.search);
     const postId = urlParams.get("id");
+    const postsContainer = document.querySelector('#postsContainer');
 
-    if (postId) {
-      renderPostTemplate(postId);
-    } else {
+    if (!postId) {
+      console.log(window.location.pathname);
       initializeSearch();
-      initializePostsPage();
+      await initializePostsPage(postsContainer);
+
+    } else if (postId) {
+      const multiplePostsContent = document.getElementById('mutiplePostsContent');
+
+      const postsContainer = document.getElementById('postsContainer');
+      
+      multiplePostsContent.classList.remove('block');
+      multiplePostsContent.classList.add('hidden');
+
+      postsContainer.classList.replace('posts-container', 'single-post');
+
+
+      await renderSinglePost(postId, postsContainer);
     }
 
-  } catch {
-    console.error('Error initalizing post page.')
+  } catch (error) {
+    console.error('Error initalizing post page.', error);
   }
 };
 

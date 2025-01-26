2d79157a5d74f1e865e3015bb9a6ca068f144ba0
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-01-26 01:38:14 +0100

    feat(dynamic nav): create loggedIn and loggedOut nav state
    #22 changing links depending on accessToken in localStorage

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ console.log("Current pathname:", window.location.pathname);
 
 import "./css/style.css";
 import router from "./js/router/index.js";
-import { setLogoutListener } from "./js/ui/global/logout";
+import { updateNavLinks } from "./js/ui/global/updateNavLinks.js";
 import { setNavToggler } from "./js/utilities/navToggler.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
   try {
+    updateNavLinks();
+  } catch (error) {
+    console.error("Error updating nav links", error);
+  }
+  
+  try {
     setNavToggler();
   } catch (error) {
     console.error("Error setting up nav", error);
-  }
-
-  try {
-    setLogoutListener();
-  } catch (error) {
-    console.error("Error setting up logout", error);
   }
 });
 
